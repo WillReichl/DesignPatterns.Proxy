c73718389c94c2b6d0412eb5fd34b5ea59c622a9
Will Reichl <William.Reichl@dcsg.com>
2020-05-18 07:51:42 -0400

    Protection proxy example

--- ProtectionProxy/Program.cs
+++ ProtectionProxy/Program.cs
@@ -2,11 +2,52 @@
 
 namespace ProtectionProxy
 {
-    class Program
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is being driven");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Driver driver;
+        private Car car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+                Console.WriteLine("too young");
+        }
+    }
+
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var car = new CarProxy(new Driver { Age = 12 });
+            car.Drive();
+
+            var car2 = new CarProxy(new Driver { Age = 16 });
+            car2.Drive();
         }
     }
 }
